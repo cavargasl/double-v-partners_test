f4c65d7ea1e1eaa8f6309a87f2995abaf9c93536
Camilo Vargas <camilovargasingm@gmail.com>
2024-09-28 21:33:35 -0500

    feat: install and configure eslintrc + prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+/** @type {import("eslint").Linter.Config} */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+    "plugin:tailwindcss/recommended",
+  ],
+  ignorePatterns: ["dist", "node_modules"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "@typescript-eslint", "tailwindcss"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+  },
+  settings: {
+    tailwindcss: {
+      config: "./tailwind.config.ts",
+    },
+  },
+}
